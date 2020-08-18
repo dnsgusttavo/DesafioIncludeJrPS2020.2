fa45a8349f7a8edade0748221da2c6996e084a6a
dnsgusttavo <gustavo.maltez@hotmail.com>
2020-08-18 11:36:56 -0300

    Added react-items-carousel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
+import ItemsCarousel from 'react-items-carousel';
+
 import './App.css';
-import { Container, Header, Main, Footer } from './styles';
+import { Container, Header, Main, Footer, CarouselItem, Chevron, Projects } from './styles';
 
 import Logo from './assets/images/logo.png';
 import GitHubLogo from './assets/images/github-icon.png'
 import LinkedInLogo from './assets/images/linkedin-icon.png'
+
+import ex1 from './assets/images/ex1.png';
+import ex2 from './assets/images/ex2.png';
+import ex3 from './assets/images/ex3.png';
+
+import chevronL from './assets/images/chevronL.png';
+import chevronR from './assets/images/chevronR.png';
+
 const App = () => {
+  const [activeItemIndex, setActiveItemIndex] = useState(0);
   return(
      <>
       <Container type="dark" id="main">
@@ -28,15 +39,52 @@ const App = () => {
       </Container>
       <Container type="light" id="about"/>
       <Container type="dark" id="tools"/>
-      <Container type="light" id="projects"/>
+
+      <Container type="light" id="projects">
+          <Projects>
+          <h1 id="title">Meus Projetos</h1>
+          <ItemsCarousel
+            requestToChangeActive={setActiveItemIndex}
+            activeItemIndex={activeItemIndex}
+            numberOfCards={3}
+            gutter={100}
+            leftChevron={<Chevron src={chevronL}/>}
+            rightChevron={<Chevron src={chevronR}/>}
+            chevronWidth={100}
+            infiniteLoop={true}
+          >
+
+            <CarouselItem>
+              <img src={ex1} alt="App Image"/>
+              <h1>Sample Text</h1>
+            </CarouselItem>
+
+            <CarouselItem>
+              <img src={ex3} alt="App Image"/>
+              <h1>Sample Text</h1>
+            </CarouselItem>
+
+            <CarouselItem>
+              <img src={ex2} alt="App Image"/>
+              <h1>Sample Text</h1>
+            </CarouselItem>
+
+            <CarouselItem>
+              <img src={ex3} alt="App Image"/>
+              <h1>Sample Text</h1>
+            </CarouselItem>
+          
+          </ItemsCarousel>
+          </Projects>
+      </Container>
 
       <Footer>
         <img src={Logo} alt="logo"/>
         <p>Copyright © 2020 Gustavo Moraes Maltez</p>
 
         <ul>
-          <li><a href="https://github.com/dnsgusttavo"><img src={GitHubLogo}/></a></li>
-          <li><a href="https://www.linkedin.com/in/gustavommaltez/"><img src={LinkedInLogo}/></a></li>
+          <li><a href="https://github.com/dnsgusttavo"><img src={GitHubLogo} alt="GitHub Logo"/></a></li>
+          <li><a href="https://www.linkedin.com/in/gustavommaltez/"><img src={LinkedInLogo} alt="LinkedIn Logo"/></a></li>
         </ul>
       </Footer>
      </>
--- src/styles.js
+++ src/styles.js
@@ -131,3 +131,36 @@ export const Footer = styled.footer`
         width: 6rem;
     }
 `;
+
+export const Chevron = styled.img`
+    width: 5rem;
+`
+export const CarouselItem = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    img{
+        height: calc(100vh * 0.6);
+    }
+
+    h1{
+        font: bold 1.3rem  'Roboto Condensed', sans-serif;
+        color: #444444;
+        margin: 10px 0;
+    }
+`
+
+export const Projects = styled.main`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    padding: 10vh 0;
+
+    h1#title{
+        text-align: center;
+        font: bold 6rem  'Roboto Condensed', sans-serif;
+        color: #444444;
+    }
+`
