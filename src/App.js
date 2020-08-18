e1547eb896aaaa67a1b01d506ce1467f90fe6fa3
dnsgusttavo <gustavo.maltez@hotmail.com>
2020-08-18 09:55:09 -0300

    Added main page style

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import './App.css';
-import { Container } from './styles';
+import { Container, Header, Main } from './styles';
 
+import Logo from './assets/images/logo.png'
 const App = () => {
   return(
      <>
-      <Container type="dark">
-        <h1>asdasdasd</h1>
+      <Container type="dark" id="main">
+        <Header>
+          <img src={Logo} alt="logo"/>
+          <ul>
+            <li><a href="#about">Sobre</a></li>
+            <li><a href="#tools">Tecnologias</a></li>
+            <li><a href="#projects">Projetos</a></li>
+            <li><a href="https://www.linkedin.com/in/gustavommaltez/">Linkedin</a></li>
+            <li><a href="https://github.com/dnsgusttavo">Github</a></li>
+          </ul>
+        </Header>
+        <Main>
+            <h1>Olá! Meu nome é {'\u00A0'}<p>Gustavo Maltez</p>.</h1>
+            <h1>Eu sou desenolvedor de software.</h1>
+            <a href="#about">ME CONHEÇA MAIS</a>
+        </Main>
+        
       </Container>
-      <Container type="light"/>
-      <Container type="dark"/>
-      <Container type="light"/>
+      <Container type="light" id="about"/>
+      <Container type="dark" id="tools"/>
+      <Container type="light" id="projects"/>
      </>
      )
 }
--- src/styles.js
+++ src/styles.js
@@ -5,3 +5,70 @@ export const Container = styled.section`
     height: 100%;
     background-color: ${props => {if(props.type === "dark") return `#26262E`; else if(props.type === "light") return '#ECF0F1'}}
 `;
+
+export const Header = styled.header`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    padding: 2rem 5%;
+    
+    img{
+        width: 20rem;
+    }
+
+    ul{
+        display: flex;
+        flex-direction: row;
+    }
+    li{
+        list-style: none;
+        padding: 0 1rem;
+    }
+    li a{
+        font: normal 2rem 'Roboto Condensed', sans-serif;
+        color: #FFF;
+        text-decoration: none;
+        transition: 200ms
+    }
+    li a:hover{
+        padding-bottom: 5px;
+        border-bottom: 0.2rem solid #37B948;
+        cursor: pointer
+    }
+`;
+
+export const Main = styled.main`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    padding-bottom: 87px;
+    h1{ 
+        display: flex;
+        font: normal 6rem 'Roboto Condensed', sans-serif;
+        color: #FFF;
+    }
+
+    h1 p{
+        color: #37B948;
+    }
+
+    a{
+        text-decoration: none;
+        color: #fff;
+        font: bold 20pt 'Roboto Condensed', sans-serif;
+        padding: 20px;
+        border-width: 3px;
+        border-style: solid;
+        border-color: #37b948;
+        margin-top: 5vh;
+        transition: 400ms;
+    }
+
+    a:hover{
+        background-color: #37b948;
+    }
+`
