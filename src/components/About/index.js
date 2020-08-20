4d95d6b96c30d09df2da72a72ee111e7f7c7c04e
dnsgusttavo <gustavo.maltez@hotmail.com>
2020-08-19 21:33:11 -0300

    Added about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
+import ProfilePic from '../../assets/images/gustavo.png'
 import { Container } from '../../styles';
+import { AboutBox, Grid, Infos } from './styles';
 
 const About = () => {
     return(
         <Container type="light" id="about">
-            <h1>asdasdasdas</h1>
+            <AboutBox>
+                
+                <img src={ProfilePic} alt="Gustavo Profile Photo"/>
+                <Infos>
+                    <h1>Sobre Mim</h1>
+                    <p>It is a long hat it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>    
+                </Infos>
+            </AboutBox>
         </Container>
     )
 }
--- src/components/About/styles.js
+++ src/components/About/styles.js
@@ -1,2 +1,34 @@
 import styled from 'styled-components';
 
+export const AboutBox = styled.section`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    padding: 0 10%;
+
+    img{
+        width: 50%;
+        border-radius: 4rem;
+    }
+    
+`
+
+export const Infos = styled.main`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    h1{
+        font: bold 6rem  'Roboto Condensed', sans-serif;
+        color: #444444;
+    }
+
+    p{
+        font: normal 3rem 'Roboto Condensed', sans-serif;
+        color: #1D1D1D;
+        padding: 4rem;
+    }
+`
